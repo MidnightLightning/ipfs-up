9848972236ea97616e9f6d257e650caccad564db
Brooks Boyd <bro@oks.me>
2017-09-12 15:41:57 -0500

    Get uploads working

--- web/js/app.js
+++ web/js/app.js
@@ -3,13 +3,14 @@ var currentHash = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn'; // Start wit
 function postToIPFS(fileObj) {
   var def = $.Deferred();
   $.ajax({
-    url: 'https://eternum.io/ipfs/' + currentHash + '/' + fileObj.name,
+    url: '/ipfs/' + currentHash + '/' + fileObj.name,
     type: 'PUT',
     data: fileObj,
     contentType: false,
     processData: false,
     success: function(data, status, jqXHR) {
-      def.resolve(data);
+      var hash = jqXHR.getResponseHeader('ipfs-hash');
+      def.resolve(hash);
     },
     error: function(jqXHR, status, error) {
       def.reject(jqXHR.status, error);
@@ -18,37 +19,69 @@ function postToIPFS(fileObj) {
   return def.promise();
 }
 
-$(document).ready(function() {
- $('#dropped-file').on('change', function(e) {
-   debugger;
- });
- $('#drop-zone').on('dragover', function(e) {
-   e.preventDefault();
-   e.stopPropagation();
-   $(this).addClass('hover');
- }).on('dragleave', function(e) {
-   e.preventDefault();
-   e.stopPropagation();
-   $(this).removeClass('hover');
- });
- $('#drop-zone').on('drop', function(e) {
-   e.preventDefault();
-   e.stopPropagation();
-   $(this).removeClass('hover');
-   var dt = e.originalEvent.dataTransfer;
-   postToIPFS(dt.files[0]);
+function addToFileList(fileObj) {
+  var $container = $('#current-upload');
+  $container.find('#file-list').append('<div>' + fileObj.name + '</div>');
+  $container.show();
+}
 
-   /*
-   for (var i = 0; i < dt.files.length; i++) {
-     var fileReader = new FileReader();
-     fileReader.onload = (function(fileData) {
-       return function(e) {
-         console.log(fileData.fileName, e.target.result);
-       }
-     })(dt.items[i]);
-     fileReader.readAsDataURL(dt.files[i]);
-     console.log(dt.files[i]);
-   }
-   */
- });
+function updateResultLinks() {
+  var $container = $('#upload-result');
+  $container.find('#upload-url').html('<a target="_top" href="/ipfs/' + currentHash + '">/ipfs/' + currentHash + '</a>');
+  $container.find('#eternum-link').attr('href', 'https://www.eternum.io/pin/' + currentHash);
+  $container.show();
+}
+
+$(document).ready(function() {
+ $('#dropped-file')
+   .on('change', function(e) {
+     debugger;
+   });
+ $('#drop-zone')
+   .on('dragover', function(e) {
+     e.preventDefault();
+     e.stopPropagation();
+     $(this).addClass('hover');
+   })
+   .on('dragleave', function(e) {
+     e.preventDefault();
+     e.stopPropagation();
+     $(this).removeClass('hover');
+   })
+   .on('drop', function(e) {
+     e.preventDefault();
+     e.stopPropagation();
+     $(this).removeClass('hover');
+     var dt = e.originalEvent.dataTransfer;
+     var fileObj = dt.files[0];
+     postToIPFS(fileObj).then(function(newHash) {
+       console.log('New Hash', newHash);
+       addToFileList(fileObj);
+       currentHash = newHash;
+       updateResultLinks();
+     }, function(err, msg) {
+       console.error(err, msg);
+    });
+
+     /*
+     for (var i = 0; i < dt.files.length; i++) {
+       var fileReader = new FileReader();
+       fileReader.onload = (function(fileData) {
+         return function(e) {
+           console.log(fileData.fileName, e.target.result);
+         }
+       })(dt.items[i]);
+       fileReader.readAsDataURL(dt.files[i]);
+       console.log(dt.files[i]);
+     }
+     */
+   });
+
+   $('#clear-btn').on('click', function(e) {
+     e.preventDefault();
+     e.stopPropagation();
+     currentHash = 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn';
+     $('#current-upload').hide().find('#file-list').html('');
+     $('#upload-result').hide();
+   });
 });
